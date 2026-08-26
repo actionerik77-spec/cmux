9d66c6163569060c583be7cb49f6b40e6cb536dc
austinpower1258 <austinwang115@gmail.com>
2026-08-26 01:57:52 -0700

    fix: clear merged-main Swift warnings

--- CLI/cmux.swift
+++ CLI/cmux.swift
@@ -33123,7 +33123,7 @@ export default CMUXSessionRestore;
         } else {
             Self.subcommandActions[subcommand] ?? .noop
         }
-        var hookResponse = cursorShellNeedsApproval
+        let hookResponse = cursorShellNeedsApproval
             ? AgentHookNotificationPolicy.cursorNativeApprovalResponse
             : "{}"
 #if DEBUG
@@ -35718,7 +35718,7 @@ export default CMUXSessionRestore;
             event["tool_input"] = source == "cursor"
                 ? sanitizedCursorFeedToolInput(toolInput)
                 : toolInput
-        } else if let cursorShellCommand {
+        } else if cursorShellCommand != nil {
             event["tool_input"] = [
                 "command": "<redacted>"
             ]
