0de1fc84d34c9196d573c8e7f7564699f35f3389
austinpower1258 <austinwang115@gmail.com>
2026-08-25 17:53:45 -0700

    fix: isolate resolved chord dispatch state

--- Sources/AppDelegate+DockShortcutRouting.swift
+++ Sources/AppDelegate+DockShortcutRouting.swift
@@ -243,6 +243,7 @@ extension AppDelegate {
     }
 
     func matchesLegacyNextSurfaceShortcut(event: NSEvent) -> Bool {
+        guard activeResolvedPrefixChordActionID == nil else { return false }
         matchTabShortcut(
             event: event,
             shortcut: StoredShortcut(key: "\t", command: false, shift: false, option: false, control: true)
@@ -250,6 +251,7 @@ extension AppDelegate {
     }
 
     func matchesLegacyPreviousSurfaceShortcut(event: NSEvent) -> Bool {
+        guard activeResolvedPrefixChordActionID == nil else { return false }
         matchTabShortcut(
             event: event,
             shortcut: StoredShortcut(key: "\t", command: false, shift: true, option: false, control: true)
@@ -285,7 +287,8 @@ extension AppDelegate {
         event: NSEvent,
         route: GhosttyGotoSplitRoute
     ) -> Bool {
-        guard !shouldBypassPrefixChordPassThrough(event) else { return false }
+        guard activeResolvedPrefixChordActionID == nil,
+              !shouldBypassPrefixChordPassThrough(event) else { return false }
         guard event.type == .keyDown,
               let shortcut = ghosttyGotoSplitShortcut(for: route),
               matchesRawGhosttyGotoSplitShortcut(event: event, shortcut: shortcut, route: route) else {
